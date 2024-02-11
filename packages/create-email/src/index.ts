56d5355cee1672f26267af082decb86488733669
gabriel miranda <gabrielmfern@outlook.com>
2024-02-11 12:32:59 -0300

    feat(create-email): use @commander-js/extra-typings for better ts support

--- packages/create-email/src/index.ts
+++ packages/create-email/src/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { program } from "commander";
+import { Argument, program } from "@commander-js/extra-typings";
 import { fileURLToPath } from "node:url";
 import fse from "fs-extra";
 import logSymbols from "log-symbols";
@@ -11,26 +11,19 @@ import path from "node:path";
 program
   .name("create-email")
   .version("0.0.19")
-  .description("The easiest way to get started with React Email");
-
-program
-  .argument("[dir]", "path to initialize the project", ".")
-  .action(async (projectPath) => {
+  .description("The easiest way to get started with React Email")
+  .addArgument(new Argument("dir", "path to initialize the project")
+     .default("react-email-starter"))
+  .action(async (relativeProjectPath) => {
     const spinner = ora("Preparing files...\n").start();
 
-    if (!projectPath) {
-      projectPath = path.join(process.cwd(), "react-email-starter");
-    }
-
-    if (typeof projectPath === "string") {
-      projectPath = projectPath.trim();
-    }
+    let projectPath = path.resolve(process.cwd(), relativeProjectPath).trim();
 
     const __dirname = path.dirname(fileURLToPath(import.meta.url));
     const templatePath = path.resolve(__dirname, "../template");
     const resolvedProjectPath = path.resolve(projectPath);
 
-    fse.copySync(templatePath, resolvedProjectPath, { recursive: true });
+    await fse.copy(templatePath, resolvedProjectPath, { recursive: true });
 
     const { report } = await treeCli({
       l: 4,
